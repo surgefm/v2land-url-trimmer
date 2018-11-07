6aef03632306d6cedb5c03138caacccf1d3e36ab
IncredibLink <simon.zehua.li@gmail.com>
2018-11-07 00:59:50 -0500

    更新 Test Cases

--- tests/trim.test.js
+++ tests/trim.test.js
@@ -5,9 +5,9 @@ const expect = require('expect');
 describe('Test URL trimmer', async function() {
   for (const testcase of Object.keys(testcases)) {
     it('#url: ' + testcase, async function() {
-      const result = await trimmer(testcase);
-      console.log(result);
-      expect(result).toEqual(testcases[testcase]);
+      const result = await trimmer.trim(testcase);
+      console.log(result.toString());
+      expect(result.toString()).toEqual(testcases[testcase]);
     }).timeout(10000);
   }
 });
