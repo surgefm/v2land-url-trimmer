0abc9522114216e50e71353b908363f0700ee2cb
IncredibLink <link@langchao.org>
2018-12-04 23:22:23 -0500

    Implement directory support

--- trimmers/index.js
+++ trimmers/index.js
@@ -3,11 +3,18 @@ const path = require('path');
 const siteTrimmerPaths = fs.readdirSync(path.resolve(__dirname, './sites'));
 
 const siteTrimmers = {};
-for (const path of siteTrimmerPaths) {
-  if (path.slice(-8) === '.site.js') {
-    const { domains, trimmer } = require(`./sites/${path}`);
+for (const p of siteTrimmerPaths) {
+  const filePath = path.resolve(__dirname, './sites', p);
+  const stat = fs.statSync(filePath);
+  if (stat.isFile() && p.slice(-8) === '.site.js') {
+    const { domains, trimmer } = require(`./sites/${p}`);
     for (const domain of domains) {
       siteTrimmers[domain] = trimmer;
+    }
+  } else if (stat.isDirectory()) {
+    const paths = fs.readdirSync(filePath);
+    for (const item of paths) {
+      siteTrimmerPaths.push(`${p}/${item}`);
     }
   }
 }
